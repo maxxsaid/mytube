afc3da8beb3f783a75a9fc6523368f1cbba19a9f
Max Said <maxsaid95@gmail.com>
2022-04-18 17:32:54 -0400

    style update for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,28 +5,30 @@ function Header(props) {
     <div className="container-fluid border-bottom-1 shadow">
       <div className="col-md-12 pt-1 pb-0">
         <nav className="navbar">
-          <a className="navbar-brand font-weight-bold text-dark" href="/">
-            myTube
-          </a>
-          <form className="col-md-7">
-            <div className="form-group d-flex">
-              <input
-                type="text"
-                name="search"
-                id="search"
-                placeholder="Search"
-                className="form-control col-md-10"
-              />
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-danger col-md-1 ml-1"
-              />
-            </div>
-          </form>
-          <p>
-            Welcome, <span className="text-primary">Guest</span>
-          </p>
+          <div class="container-fluid">
+            <a className="navbar-brand font-weight-bold text-dark" href="/">
+              myTube
+            </a>
+            <form className="col-md-7">
+              <div className="form-group d-flex">
+                <input
+                  type="text"
+                  name="search"
+                  id="search"
+                  placeholder="Search"
+                  className="form-control me-2"
+                />
+                <input
+                  type="submit"
+                  value="Search"
+                  className="btn btn-outline-danger col-md-1 ml-1"
+                />
+              </div>
+            </form>
+            <p>
+              Welcome, <span className="text-danger">Guest</span>
+            </p>
+          </div>
         </nav>
       </div>
     </div>
