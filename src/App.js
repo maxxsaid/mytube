4c5f0b5a76928a117d29780b34ad22b08f1a7de8
Max Said <maxsaid95@gmail.com>
2022-04-20 14:41:51 -0400

    search bar, alert on submit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header";
 
 function App() {
+  // search item
+  const [search, setSearch] = useState("");
+  // searched data
+  const [data, setData] = useState("");
+  // function to retrieve data from API
+  const searchData = (text) => {
+    alert(text);
+  };
   return (
     <div className="App">
+      <Header search={searchData} />
       <h1>myTube</h1>
-      <Header />
       {/* body */}
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Header(props) {
+  // input data
+  const [text, setText] = useState("");
+  // onSubmit function
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.search(text);
+  };
   return (
     <div className="container-fluid border-bottom-1 shadow">
       <div className="col-md-12 pt-1 pb-0">
@@ -9,7 +16,7 @@ function Header(props) {
             <a className="navbar-brand font-weight-bold text-dark" href="/">
               myTube
             </a>
-            <form className="col-md-7">
+            <form className="col-md-7" onSubmit={(e) => handleSubmit(e)}>
               <div className="form-group d-flex">
                 <input
                   type="text"
@@ -17,6 +24,8 @@ function Header(props) {
                   id="search"
                   placeholder="Search"
                   className="form-control me-2"
+                  value={text}
+                  onChange={(e) => setText(e.target.value)}
                 />
                 <input
                   type="submit"
